2c700011fbc151ffa0daf484f2dd079facb171d2
steveazzolin <steve.azzolin1@gmail.com>
2022-12-29 14:32:18 +0100

    fix

--- actions/actions.py
+++ actions/actions.py
@@ -390,8 +390,8 @@ class ActionGetCompanyInfo(Action):
             print(e)
             dispatcher.utter_message(text="The maximum number of requests to the API has been reached. Try again tomorrow.")    
             return []
-            
-        dispatcher.utter_message(text=info)
+
+        dispatcher.utter_message(text=f"Here the description of {company_name}:" + info)
         return []
 
 
